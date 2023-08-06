6e6eba964893fd856dc68967ba8183d8382472eb
JackAngione <jangio1@students.towson.edu>
2023-08-06 04:03:29 -0400

    created class for each chess piece
    each type of chess piece now has it's own class, with methods to initialize it's color/position.
    methods to move the piece, and check if proposed move is valid

--- jack_chess/src/ChessPiece.java
+++ jack_chess/src/ChessPiece.java
@@ -1,27 +1,22 @@
 public interface ChessPiece {
+    char color = 'w';
     int pos_x = 0;
     int pos_y = 0;
     void move(int x, int y);
     boolean isValidMove(int x, int y);
-
+    //TODO possibly make a getter for position
 }
 
 class King implements ChessPiece
 {
+    char color;
     int pos_x;
     int pos_y;
-    public King(Character color)
+    public King(char color, int x, int y)
     {
-        this.pos_x = 4;
-        if(color == 'w' )
-        {
-            this.pos_y = 0;
-        }
-        else
-        {
-            this.pos_y = 7;
-        }
-
+        this.color = color;
+        this.pos_x = x;
+        this.pos_y = y;
     }
     @Override
     public void move(int x, int y) {
@@ -32,19 +27,214 @@ class King implements ChessPiece
         }
     }
 
-
     //takes in where the piece wants to move
     @Override
     public boolean isValidMove(int x, int y) {
         //the x move is within one space
-        if(pos_x-1 >= x-1 && pos_x+1 < x+1)
+        if(pos_x-1==x || pos_x+1 ==x)
         {
-            //y move is within one space
-            if(pos_y-1 )
+            if(pos_y-1 == y || pos_y+1 == y)
             {
-
+                //valid x and y move
+                return true;
             }
         }
+        return false;
+    }
+}
+
+class Queen implements ChessPiece
+{
+    char color;
+    int pos_x;
+    int pos_y;
+    public Queen(char color, int x, int y)
+    {
+        this.color = color;
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+    @Override
+    public void move(int x, int y) {
+        if(isValidMove(x,y))
+        {
+            //set new position
+            this.pos_x = x;
+            this.pos_y = y;
+        }
+    }
+
+    //takes in where the piece wants to move
+    @Override
+    public boolean isValidMove(int x, int y) {
+        if(x == pos_x || y== pos_y)
+        {
+            //then movement is on axis, like a rook
+            return true;
+        }
+
+        // Calculate the absolute differences in x and y coordinates
+        int dx = Math.abs(x - pos_x);
+        int dy = Math.abs(y - pos_y);
+
+        // Check if the move is diagonal
+        return dx == dy;
+    }
+}
+
+
+class Rook implements ChessPiece
+{
+    char color;
+    int pos_x;
+    int pos_y;
+    public Rook(char color, int x, int y)
+    {
+        this.color = color;
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+    @Override
+    public void move(int x, int y) {
+        if(isValidMove(x,y))
+        {
+            this.pos_x = x;
+            this.pos_y = y;
+        }
+    }
+
+    //takes in where the piece wants to move
+    @Override
+    public boolean isValidMove(int x, int y) {
+
+        if(x == pos_x || y == pos_y)
+        {
+            //then movement is on axis
+            return true;
+        }
+        return false;
+    }
+}
+class Bishop implements ChessPiece
+{
+    char color;
+    int pos_x;
+    int pos_y;
+    public Bishop(char color, int x, int y)
+    {
+        this.color = color;
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+    @Override
+    public void move(int x, int y) {
+        if(isValidMove(x,y))
+        {
+            this.pos_x = x;
+            this.pos_y = y;
+        }
+    }
+
+    //takes in where the piece wants to move
+    @Override
+    public boolean isValidMove(int x, int y) {
+
+        // Calculate the absolute differences in x and y coordinates
+        int dx = Math.abs(x - pos_x);
+        int dy = Math.abs(y - pos_y);
+
+        // Check if the move is diagonal
+        return dx == dy;
+    }
+}
+
+class Knight implements ChessPiece
+{
+    char color;
+    int pos_x;
+    int pos_y;
+    public Knight(char color, int x, int y)
+    {
+        this.color = color;
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+    @Override
+    public void move(int x, int y) {
+        if(isValidMove(x,y))
+        {
+            this.pos_x = x;
+            this.pos_y = y;
+        }
+    }
+
+    //takes in where the piece wants to move
+    @Override
+    public boolean isValidMove(int x, int y) {
+        // Calculate the absolute differences in x and y coordinates
+        int dx = Math.abs(x - pos_x);
+        int dy = Math.abs(y - pos_y);
+
+        // Check if it's a valid knight move
+        return (dx == 1 && dy == 2) || (dx == 2 && dy == 1);
+    }
+}
+class Pawn implements ChessPiece
+{
+    char color;
+    int pos_x;
+    int pos_y;
+    public Pawn(char color, int x, int y)
+    {
+        this.color = color;
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+    @Override
+    public void move(int x, int y) {
+        if(isValidMove(x,y))
+        {
+            this.pos_x = x;
+            this.pos_y = y;
+        }
+    }
+
+    //takes in where the piece wants to move
+    @Override
+    public boolean isValidMove(int x, int y) {
+        //white
+        if(this.color == 'w')
+        {
+            //pawn first move, can go two spaces
+            if(y==1)
+            {
+                if(x == pos_x && y == pos_y-2)
+                {
+                    return true;
+                }
+            }
+            if(x == pos_x && y == pos_y-1)
+            {
+                return true;
+            }
+        }
+        //black
+        else
+        {
+            //pawn first move, can go two spaces
+            if(y==1)
+            {
+                if(x == pos_x && y == pos_y+2)
+                {
+                    return true;
+                }
+            }
+            if(x == pos_x && y == pos_y+1)
+            {
+                return true;
+            }
+        }
+        //TODO add diagonal take move for pawn
         return false;
     }
 }
